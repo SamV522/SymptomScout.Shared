2a29f32075c6b7f5f38bc8e3a7ba994eb7c97760
Sam Vlahos <Sam.V_@hotmail.com>
2023-06-22 14:31:16 +1000

    Initial commit

---
+++ Models/Diagnosis.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SymptomScout.Shared.Models
+{
+    public class Diagnosis
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DiagnosisId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [InverseProperty("Diagnosis")]
+        public ICollection<Symptom> Symptoms { get; set; }
+    }
+}
---
+++ Models/Symptom.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SymptomScout.Shared.Models
+{
+    public class Symptom
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SymptomId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Diagnosis> Diagnoses { get; set; }
+    }
+}
